9c9956ec1c0ecfa61f817c405ec07d884a0c165d
Jonathan Lam <jlam55555@gmail.com>
2016-03-19 16:42:44 -0400

    Made meta tags and header dynamically loaded

--- public/script.js
+++ public/script.js
@@ -1,5 +1,30 @@
 $(function() {
 
+	// add meta tags
+	var metas = `
+	<meta charset="utf-8" />
+	<meta name="description" content="The official website for the Barlow Programming team" />
+	<meta name="keywords" content="joel barlow, barlow, jbhs, programming, coding" />
+	<meta name="author" content="Jonathan Lam <jlam55555@gmail.com>" />
+	<link rel="icon" type="img/x-icon" href="favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	`;
+	$("head").append(metas);
+
+	// add header
+	var header = `
+	<div id="header">
+		<div id="title">JBHS Programming Team</div>	
+		<nav id="links">
+			<a class="animate" href="index.html">Home</a>
+			<a class="animate" href="projects.html">Projects</a>
+			<a class="animate" href="team.html">Team</a>
+			<a class="animate" href="contact.html">Contact</a>
+		</nav>
+	</div>
+	`;
+	$("body").prepend(header);
+
 	// title animation
 	var titleElem = $("div#title");
 	var title = titleElem.text();
